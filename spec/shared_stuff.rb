d3e8acf1804df4a45606e3f6655725e60a3177e7
prudnikovv <prudnikovv@ukr.net>
2022-08-11 15:38:36 +0000

    Impruve rspec

--- spec/shared_stuff.rb
+++ spec/shared_stuff.rb
@@ -5,7 +5,7 @@ RSpec.shared_context "shared stuff", shared_context: :metadata do
   let(:game) { Codebreaker::Game.new }
   let(:winner) { Codebreaker::Winner.new("Vlad", game) }
   before(:each) do
-    game.user("Vlad")
-    game.set_difficulty("Easy")
+    game.user_init("Vlad")
+    game.difficulty_init("Easy")
   end
 end
